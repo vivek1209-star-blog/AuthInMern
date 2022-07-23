846ac840f62709e175ad712d43ed1815a61ccb43
vivek mishra <52.2018.vivekmishra@gmail.com>
2022-07-23 14:56:35 +0530

    added refresh token and logout

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const router = require("./routes/user-routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
 mongoose
   .connect(
-    `mongodb+srv://vivek1209:mLLd6DdnoEeJ55Zy@cluster0.nojb4cx.mongodb.net/mern-auth?retryWrites=true&w=majority`
+    `mongodb+srv://vivek1209:${process.env.MONGODB_PASSWORORD}@cluster0.nojb4cx.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
     console.log("Database is connected! Listening to localhost 5000");
   })
   .catch((err) => console.log(err));
-
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,8 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET_KEY = "MyKey"
 const signup = async (req, res, next) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
   let existingUser;
   try {
     existingUser = await User.findOne({ email: email });
@@ -22,50 +21,59 @@ const signup = async (req, res, next) => {
     password: hashedPassword,
   });
 
-    try {
-        await user.save();
-    }catch(err){
-   console.log(err)
-    }
-   
-    return res.status(201).json({message:user})
+  try {
+    await user.save();
+  } catch (err) {
+    console.log(err);
+  }
+  return res.status(201).json({ message: user });
 };
 
 const login = async (req, res, next) => {
-    const { email, password } = req.body;
-  
-    let existingUser;
-    try {
-      existingUser = await User.findOne({ email: email });
-    } catch (err) {
-      return new Error(err);
-    }
-    if (!existingUser) {
-      return res.status(400).json({ message: "User not found. Signup Please" });
-    }
-    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
-    if (!isPasswordCorrect) {
-      return res.status(400).json({ message: "Inavlid Email / Password" });
-    }
+  const { email, password } = req.body;
 
-    const token = jwt.sign({ id: existingUser._id }, JWT_SECRET_KEY, {
-        expiresIn: "35s",
-      });
-    
-      console.log("Generated Token\n", token);
-    return res
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    return new Error(err);
+  }
+  if (!existingUser) {
+    return res.status(400).json({ message: "User not found. Signup Please" });
+  }
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Inavlid Email / Password" });
+  }
+  const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "35s",
+  });
+
+  console.log("Generated Token\n", token);
+
+  if (req.cookies[`${existingUser._id}`]) {
+    req.cookies[`${existingUser._id}`] = "";
+  }
+
+  res.cookie(String(existingUser._id), token, {
+    path: "/",
+    expires: new Date(Date.now() + 1000 * 30), // 30 seconds
+    httpOnly: true,
+    sameSite: "lax",
+  });
+
+  return res
     .status(200)
-    .json({ message: "Successfully Logged In", user: existingUser, token })
-
-}
+    .json({ message: "Successfully Logged In", user: existingUser, token });
+};
 
 const verifyToken = (req, res, next) => {
-const headers = req.headers['authorization'];
-const token = headers.split(" ")[1];
-if (!token) {
+  const cookies = req.headers.cookie;
+  const token = cookies.split("=")[1];
+  if (!token) {
     res.status(404).json({ message: "No token found" });
   }
-  jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+  jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
     if (err) {
       return res.status(400).json({ message: "Invalid TOken" });
     }
@@ -73,25 +81,72 @@ if (!token) {
     req.id = user.id;
   });
   next();
-  };
+};
 
-  const getUser = async (req, res, next) => {
-    const userId = req.id;
-    let user;
-    try {
-      user = await User.findById(userId, "-password");
-    } catch (err) {
-      return new Error(err);
+const getUser = async (req, res, next) => {
+  const userId = req.id;
+  let user;
+  try {
+    user = await User.findById(userId, "-password");
+  } catch (err) {
+    return new Error(err);
+  }
+  if (!user) {
+    return res.status(404).json({ messsage: "User Not FOund" });
+  }
+  return res.status(200).json({ user });
+};
+const refreshToken = (req, res, next) => {
+  const cookies = req.headers.cookie;
+  const prevToken = cookies.split("=")[1];
+  if (!prevToken) {
+    return res.status(400).json({ message: "Couldn't find token" });
+  }
+  jwt.verify(String(prevToken), process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.status(403).json({ message: "Authentication failed" });
     }
-    if (!user) {
-      return res.status(404).json({ messsage: "User Not FOund" });
+    res.clearCookie(`${user.id}`);
+    req.cookies[`${user.id}`] = "";
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "35s",
+    });
+    console.log("Regenerated Token\n", token);
+
+    res.cookie(String(user.id), token, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 30), // 30 seconds
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    req.id = user.id;
+    next();
+  });
+};
+
+const logout = (req, res, next) => {
+  const cookies = req.headers.cookie;
+  const prevToken = cookies.split("=")[1];
+  if (!prevToken) {
+    return res.status(400).json({ message: "Couldn't find token" });
+  }
+  jwt.verify(String(prevToken), process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.status(403).json({ message: "Authentication failed" });
     }
-    return res.status(200).json({ user });
-  };
+    res.clearCookie(`${user.id}`);
+    req.cookies[`${user.id}`] = "";
+    return res.status(200).json({ message: "Successfully Logged Out" });
+  });
+};
 
-
+exports.logout = logout;
 exports.signup = signup;
 exports.login = login;
 exports.verifyToken = verifyToken;
 exports.getUser = getUser;
-
+exports.refreshToken = refreshToken;
