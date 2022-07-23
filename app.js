20c526202aca09e32f8856f4bcca686522c7455b
vivek mishra <52.2018.vivekmishra@gmail.com>
2022-07-23 11:22:03 +0530

    added mongoose and connected to database

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
- const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const router = require("./routes/user-routes");
 
+app.use("/api", router);
+mongoose
+  .connect(
+    `mongodb+srv://vivek1209:mLLd6DdnoEeJ55Zy@cluster0.nojb4cx.mongodb.net/mern-auth?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("Database is connected! Listening to localhost 5000");
+  })
+  .catch((err) => console.log(err));
 
- app.use('/', (req, res, next) => {
-    console.log("Hello Friend");
- })
-
- app.listen(5000, () => {
-    console.log("Listening to localhost 5000")
- })
