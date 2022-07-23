19fe7f85d7035e6801d676fa0e89997bbdc5234d
vivek mishra <52.2018.vivekmishra@gmail.com>
2022-07-23 12:54:33 +0530

    completed jwt token verify and get token

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const User = require("../model/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = "MyKey"
 const signup = async (req, res, next) => {
     const { name, email, password } = req.body;
   let existingUser;
@@ -28,6 +29,69 @@ const signup = async (req, res, next) => {
     }
    
     return res.status(201).json({message:user})
+};
+
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    let existingUser;
+    try {
+      existingUser = await User.findOne({ email: email });
+    } catch (err) {
+      return new Error(err);
+    }
+    if (!existingUser) {
+      return res.status(400).json({ message: "User not found. Signup Please" });
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Inavlid Email / Password" });
+    }
+
+    const token = jwt.sign({ id: existingUser._id }, JWT_SECRET_KEY, {
+        expiresIn: "35s",
+      });
+    
+      console.log("Generated Token\n", token);
+    return res
+    .status(200)
+    .json({ message: "Successfully Logged In", user: existingUser, token })
+
 }
 
+const verifyToken = (req, res, next) => {
+const headers = req.headers['authorization'];
+const token = headers.split(" ")[1];
+if (!token) {
+    res.status(404).json({ message: "No token found" });
+  }
+  jwt.verify(String(token), JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(400).json({ message: "Invalid TOken" });
+    }
+    console.log(user.id);
+    req.id = user.id;
+  });
+  next();
+  };
+
+  const getUser = async (req, res, next) => {
+    const userId = req.id;
+    let user;
+    try {
+      user = await User.findById(userId, "-password");
+    } catch (err) {
+      return new Error(err);
+    }
+    if (!user) {
+      return res.status(404).json({ messsage: "User Not FOund" });
+    }
+    return res.status(200).json({ user });
+  };
+
+
 exports.signup = signup;
+exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser;
+
